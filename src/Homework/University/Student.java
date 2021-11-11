3d8f4375df216e1b62ede9c12a6401aa0963d6e0
crdiam <angiziewska@gmail.com>
2021-11-11 12:22:08 +0100

    methods addStudent(s), removeStudent(s), transferStudent(from, to, s) implemented

--- src/Homework/University/Student.java
+++ src/Homework/University/Student.java
@@ -11,4 +11,15 @@ public class Student extends Person {
     public String getIndexNumber() {
         return indexNumber;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + getName() + '\'' +
+                "surname='" + getSurname() + '\'' +
+                "date of birth'" + getBirthDate() + '\'' +
+                "PESEL'" + getPESEL() + '\'' +
+                "indexNumber='" + indexNumber + '\'' +
+                '}';
+    }
 }
